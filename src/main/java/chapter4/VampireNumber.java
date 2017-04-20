e45aa0ec5c7b714797e31628dbb412c5ed83d904
zhangbj <zhangbj@guahao.com>
2017-04-20 15:04:01 +0800

    吸血鬼数字

--- src/main/java/chapter4/VampireNumber.java
+++ src/main/java/chapter4/VampireNumber.java
@@ -1,5 +1,9 @@
 package chapter4;
 
+import org.apache.commons.lang.ArrayUtils;
+
+import java.util.Arrays;
+
 /**
  * @author zhangbj
  * @version 1.0
@@ -10,13 +14,20 @@ package chapter4;
 
 public class VampireNumber {
 
-    public void getVampire(Integer digit){
+    public static void getVampire(Integer digit){
         int from ;
         int to;
+        int count = 0;
         int multDigit = (int) Math.pow(10,digit / 2 - 1);
+        int fromDigit = (int) Math.pow(10,digit -1);
+        int toDigit1 = (int) Math.pow(10,digit);
+        int toDigit2 = (int) Math.pow(10,digit / 2);
+
         for (int index = multDigit;index < 10 * multDigit;index ++){
-            from = Math.max(10 * multDigit / index,index + 1);
-            to = Math.min(100 * multDigit / index,10 * multDigit);
+            /*from = Math.max(1000 / i, i + 1);
+            to = Math.min(10000 / i, 100);*/
+            from = Math.max(fromDigit / index,index + 1);
+            to = Math.min(toDigit1 / index,toDigit2);
             for (int jndex = from; jndex < to;jndex++){
 
                 int max = index * jndex;
@@ -24,12 +35,21 @@ public class VampireNumber {
                 if (max % 100 == 0)
                     continue;
 
-                String[] StringMax = String.valueOf(max).split("");
-                String[] StringMult =  String.valueOf(index).split("") + String.valueOf(jndex).split("");
+                char[] charsMax = String.valueOf(max).toCharArray();
+                char[] charsMult = ArrayUtils.addAll(String.valueOf(index).toCharArray(),String.valueOf(jndex).toCharArray());
 
+                Arrays.sort(charsMax);
+                Arrays.sort(charsMult);
 
-
+                if (Arrays.equals(charsMax,charsMult)){
+                    count++;
+                    System.out.println("第" + count + "组吸血鬼数字为 ：" + index + " x " + jndex + " = " + max);
+                }
             }
         }
+    }
+
+    public static void main(String[] args) {
+        VampireNumber.getVampire(6);
     }
 }
