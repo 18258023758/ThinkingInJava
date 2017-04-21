0532890f328143a9cf8f7c0c743d76febf0a32cc
zhangbj <zhangbj@guahao.com>
2017-04-21 15:14:42 +0800

    test-branch

---
+++ src/main/java/chapter5/TestClass.java
@@ -0,0 +1,27 @@
+package chapter5;
+
+/**
+ * @author zhangbj
+ * @version 1.0
+ * @Type TestClass
+ * @Desc
+ * @data 2017/4/21
+ */
+
+public class TestClass {
+
+    public TestClass(){
+        return;
+    }
+
+
+
+    public void info(){
+
+    }
+
+    public void info(String s){
+        //test-branch
+    }
+
+}
